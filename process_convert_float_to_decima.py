42861b48ac169be544adc83a1498397019d1ec95
nguyenduc1222 <nguyenduc1222@gmail.com>
2019-07-07 14:19:30 +0700

    Update process_convert_float_to_decima.py

--- process_convert_float_to_decima.py
+++ process_convert_float_to_decima.py
@@ -2,14 +2,16 @@ import sys
 import fileinput
 from decimal import *
 
-# replace all occurrences of 'sit' with 'SIT' and insert a line after the 5th
-stt = 0
-for i, line in enumerate(fileinput.input('xaw2', inplace=1)):
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+
+
+for i, line in enumerate(fileinput.input(sys.argv[1], inplace=1)):
     newLine = line.replace('\n', '')
     data = newLine.split(",")
     
-    data[1] = str(Decimal(data[1]))
-    data[2] = str(Decimal(data[2]))
+    data[1] = str(Decimal(float(data[1])))
+    data[2] = str(Decimal(float(data[2])))
     data[0] = data[2] + "_" + data[1]
     
     newLine = data[0] + "," + data[1] + "," + data[2] + "\n"
